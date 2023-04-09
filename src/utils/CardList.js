364697bc6aba2f843704bce4f618d6cdadac0445
Abhijith N <abhijithnairh5555@gmail.com>
2023-04-10 02:59:33 +0530

    Image Icon changed to links
    Netlify path error images were not loading hence images hosted on Internet and used the link.

--- src/utils/CardList.js
+++ src/utils/CardList.js
@@ -1,24 +1,24 @@
 let CardList = [
 	{
-		icon: "./icons/safe.png",
+		icon: "https://imghost.net/ib/A2AiUFQ2znvU7kq_1681075398.png",
 		name: "Total Revenues",
 		value: "$2,129,430",
 		color: "#DDEFE0"
 	},
 	{
-		icon: "./icons/tag.png",
+		icon: "https://imghost.net/ib/iQIxTjXFbj4Fg0K_1681075398.png",
 		name: "Total Transactions",
 		value: "1520",
 		color: "#F4ECDD"
 	},
 	{
-		icon: "",
+		icon: "https://imghost.net/ib/PcQND2pyWjv8zB1_1681075398.png",
 		name: "Total Likes",
 		value: "9721",
 		color: "#EFDADA"
 	},
 	{
-		icon: "./icons/users.png",
+		icon: "https://imghost.net/ib/fWe7IuFyF2O0lh8_1681075667.png",
 		name: "Total Users",
 		value: "892",
 		color: "#DEE0EF"
