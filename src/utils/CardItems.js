a272b5e733ec74bc17b0a5d6237b4525e6a3a823
Abhijith N <abhijithnairh5555@gmail.com>
2023-04-10 02:20:57 +0530

    Icons not showing

--- src/utils/CardItems.js
+++ src/utils/CardItems.js
@@ -1,24 +1,29 @@
+// import { SafeIcon } from "../../public/icons/VectorSafe_icon.png"
+// import { TagIcon } from "../../public/icons/VectorTagBlack_icon.png"
+// import { LikeIcon } from "../../public/icons/VectorLike.png"
+// import { UserIcon } from "../../public/icons/VectorUsers_icon.png"
+
 let CardList = [
 	{
-		icon: "../src/assets/icons/VectorLike.png",
+		icon: "../../public/icons/VectorSafe_icon.png",
 		name: "Total Revenues",
 		value: "$2,129,430",
 		color: "#DDEFE0"
 	},
 	{
-		icon: "../src/assets/icons/VectorTagBlack_icon.png",
+		icon: "../../public/icons/VectorTagBlack_icon.png",
 		name: "Total Transactions",
 		value: "1520",
 		color: "#F4ECDD"
 	},
 	{
-		icon: "../src/assets/icons/VectorLike.png",
+		icon: "../../public/icons/VectorLike.png",
 		name: "Total Likes",
 		value: "9721",
 		color: "#EFDADA"
 	},
 	{
-		icon: "../src/assets/icons/VectorUsers_icon.png",
+		icon: "../../public/icons/VectorUsers_icon.png",
 		name: "Total Users",
 		value: "892",
 		color: "#DEE0EF"
